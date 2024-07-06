b9abcaae0d492ce44bf0513bfb375b6b6c57faf6
Benjamin Thyreau <benjamin.thyreau@gmail.com>
2024-07-06 15:37:25 +0900

    run_all.sh compile the result into a file

--- run_all.sh
+++ run_all.sh
@@ -9,11 +9,13 @@ fi
 
 outfiles=()
 for a in $@; do
-	if [ ${a} != ${a/slab_roi/} ]; then echo "ignoring output file ${a}"; continue; fi
-	if [ ${a} != ${a/brain_mask/} ]; then echo "ignoring output file ${a}"; continue; fi
+	if [ ${a} != ${a/slab_roi/} ]; then echo "ignoring file ${a}"; continue; fi
+	if [ ${a} != ${a/brain_mask/} ]; then echo "ignoring file ${a}"; continue; fi
 	$scriptpath/run.sh ${a}
 	outfiles+=(${a})
 done
 
 pth=$(dirname ${a})
 for a in ${outfiles[@]}; do cat ${a/.nii.gz/_index_webp.html}; echo "<hr>"; done > "${pth}/index.html"
+echo "filename,left_masseter_volume,right_masseter_volume,left_masseter_inslab_volume,right_masseter_inslab_volume" > "${pth}/all_masseter_volumes.csv"
+for a in ${outfiles[@]}; do echo -n "$a,"; tail -n 1 "${a/.nii.gz/_masseter_volumesLR.csv}"; done >> "${pth}/all_masseter_volumes.csv"
